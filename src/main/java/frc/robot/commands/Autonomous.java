c7886546beb7ad835d680e5f94c36f215732c39c
jsbondar <55658394+jsbondar@users.noreply.github.com>
2020-02-27 14:37:31 -0500

    Initial commit

---
+++ src/main/java/frc/robot/commands/Autonomous.java
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.Indexer;
+import frc.robot.commands.IndexerCommand;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/latest/docs/software/commandbased/convenience-features.html
+public class Autonomous extends SequentialCommandGroup {
+  /**
+   * Creates a new Autonomous.
+   */
+  public Autonomous(Indexer index) {
+    // Add your commands in the super() call, e.g.
+    // super(new FooCommand(), new BarCommand());
+    super(new IndexerCommand(index));
+  }
+}
